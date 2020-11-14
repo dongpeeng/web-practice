5bd0c3ea1e4ddec14b6cf819800f8e78efb54a6a
chen <1085295870@qq.com>
2020-11-14 20:03:17 +0800

    jquery content

---
+++ jQuery/server.php
@@ -0,0 +1,13 @@
+ <?php
+
+    header('content-type:application/json;charset=utf-8');
+    //1.接受参数
+     $username= $_POST['username'];
+    //2.进行处理
+    $users=array(
+        array("id"=>11,"name"=>"lisi","gender"=>"男"),
+        array("id"=>22,"name"=>"wangwu","gender"=>"女")
+  );
+  echo json_encode($users);
+  // phpinfo(); 
+?> 
